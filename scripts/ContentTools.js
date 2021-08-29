d29f322fec04c7606656a60a1d7e13da5e43f600
Caleb Stride <22614395+CalebStride@users.noreply.github.com>
2021-08-29 21:04:56 +0100

    Move links to normal web pages

--- scripts/ContentTools.js
+++ scripts/ContentTools.js
@@ -1,27 +1,5 @@
 class ContentTools {
 
-    // Send a get request to the html page needed
-	static getPage(sendLocation, createDocNavBar = false) {
-        this.removeChildren("dynamicContent");
-	    var xhttp = new XMLHttpRequest();
-
-	    // Decide what to do when the state changes
-	    xhttp.onreadystatechange = function() {
-	        // If response ready and http request OK
-	        if (this.readyState === 4 && this.status === 200) {
-	            document.getElementById("dynamicContent").innerHTML = this.responseText;
-                window.localStorage.setItem("currentPage", sendLocation);
-                if (createDocNavBar) {
-                    PageSummary.createNavMenuForPage("docMenuArea");
-                }
-	        }
-	    };  
-
-	    // Decide whether to post or get the values
-		xhttp.open("GET", sendLocation, true);
-		xhttp.send();
-	}
-
     // Remove all child elements of an element
     static removeChildren(elementId) {
         let element = document.getElementById(elementId);
@@ -93,13 +71,106 @@ class ContentTools {
 
     // Get the map of webpages on the website
     static getWebPagesMap() {
-        return [{"page" : "content/home.html", "name" : "Home"},
-                {"page" : "content/about.html", "name" : "About"},
-                {"page" : "content/projects.html", "name" : "Projects", "children" : [
-                    {"page" : "content/projects/project1.html", "name" : "Project One"},
-                    {"page" : "content/projects.html", "name" : "Projects"}
+        return [{"page" : "index.html", "name" : "Home"},
+                {"page" : "about.html", "name" : "About"},
+                {"page" : "projects.html", "name" : "Projects", "children" : [
+                    {"page" : "projects/project1.html", "name" : "Project One"},
+                    {"page" : "projects.html", "name" : "Projects"}
                 ]},
-                {"page" : "content/contact.html", "name" : "Contact"}];
+                {"page" : "contact.html", "name" : "Contact"}];
+    }
+
+
+    // Create the web page nav bar that's used on each page
+    static createMainNavBar() {
+        const placeArea = document.body;
+        placeArea.insertBefore(this.createBurgerMenu(), placeArea.firstChild);
+
+        let menu = document.createElement("nav");
+        menu.id = "sideBar";
+        menu.className = "hiddenFeatureMob";
+
+        menu.appendChild(this.createCloseButton());
+        menu.appendChild(this.creatNavBarFromLayout(this.getWebPagesMap()));
+        placeArea.insertBefore(menu, placeArea.firstChild);
+    }
+
+    // Create the nav bar from the given web page layout
+    static creatNavBarFromLayout(webLayout) {
+        let menuHolder = document.createElement("ul");
+        menuHolder.id = "mainNavBar";
+        this.createNavBarButtons(webLayout, menuHolder);
+        return menuHolder;
+    }
+
+    // Creates the nav bar buttons and adds them to the elementParent
+    static createNavBarButtons(webLayoutArray, elementParent) {
+        for (let i = 0; i < webLayoutArray.length; i++) {
+            const pageObject = webLayoutArray[i];
+            let menuButton = document.createElement("li");
+            if (pageObject.children == undefined) {
+
+                let buttonContent = document.createElement("div");
+                buttonContent.onclick = function(){
+                    ContentTools.toggleSelected(this);
+                };
+                buttonContent.appendChild(this.createLinkText(pageObject.name, pageObject.page));
+                menuButton.appendChild(buttonContent);
+            } else {
+
+                let buttonDropdown = document.createElement("div");
+                buttonDropdown.className = "dropDownButton";
+                buttonDropdown.appendChild(this.createLinkText(pageObject.name, pageObject.page));
+                buttonDropdown.onclick = function(){
+                    ContentTools.hideOrShowNavBar(this, pageObject.name + 'DropDown');
+                };
+                menuButton.appendChild(buttonDropdown);
+                let dropDownContent = document.createElement("ul");
+                dropDownContent.classList.add("dropDownContent", "hiddenFeature");
+                dropDownContent.id = pageObject.name + 'DropDown';
+                this.createNavBarButtons(pageObject.children, dropDownContent);
+                menuButton.appendChild(dropDownContent);
+            }
+            elementParent.appendChild(menuButton);
+        }
+    }
+
+    // Creates a link with the given text
+    static createLinkText(linkText, link) {
+        let linkTag = document.createElement('a');
+        linkTag.href = link;
+        linkTag.textContent = linkText;
+        return linkTag;
+    }
+
+    // Creates the close button used on the mobile menu
+    static createCloseButton() {
+        let closeButton = document.createElement("div");
+        closeButton.id = "closeContainer";
+        closeButton.className = "hiddenFeatureBig";
+        closeButton.onclick = function(){ContentTools.hideOrShow('sideBar', true);};
+        closeButton.textContent = "&times;";
+        return closeButton;
+    }
+
+    // Creates the burger menu when used on mobile
+    static createBurgerMenu() {
+        let burgerMenu = document.createElement("div");
+        burgerMenu.id = "burgerHug";
+
+        let burgerLineHolder = document.createElement("div");
+        burgerLineHolder.id = "b_menu";
+        burgerLineHolder.className = "hiddenFeatureBig";
+        burgerLineHolder.onclick = function(){ContentTools.hideOrShow('sideBar', true);};
+
+        for (let i = 0; i < 3; i++) {
+            let burgerLine = document.createElement("div");
+            burgerLine.className = "b_men";
+            burgerLineHolder.appendChild(burgerLine);
+        }
+
+        burgerMenu.appendChild(burgerLineHolder);
+        return burgerMenu;
     }
 
 }
