90896b62e71c55a0894f577edab6cabdd2475988
Caleb Stride <22614395+CalebStride@users.noreply.github.com>
2021-12-05 17:29:12 +0000

    Add way to generate tab names from pages

--- renderResources.js
+++ renderResources.js
@@ -1,7 +1,6 @@
 // Create reference instance
 var marked = require('marked');
 var fs = require('fs');
-var path = process.cwd();
 
 // Used to get the files that need formatting
 var getFilesToPrepare = function () {
@@ -32,26 +31,38 @@ var readAndAddResource = function (directory, listOfResources) {
 	}
 }
 
-// Read the template that's needed for all md pages and clean/create the website dir
-let template;
-try {
-	template = fs.readFileSync(path + '/resources/template.html', 'utf8');
-	console.log('Read the template file');
-} catch (err) {
-	console.error(err);
-	return;
+var findPageTitle = function (pageContents) {
+	let title = pageContents.match(/h1(.*)<\/h1>/g);
+	if (title != null && title.length == 1) {
+		title = title[0].substring(title[0].indexOf('>') + 1, title[0].indexOf('<'));
+	}
+	return title;
 }
 
+//////////// SCRIPT STARTS HERE ///////////////////
+// Remove the website directory if it exists
+const path = process.cwd();
 try {
-	fs.rmdirSync(path + '\\website');
+	fs.rmSync(path + '\\website', {recursive: true, force: true });
 	console.log('Cleared website directory');
 } catch (err) {
 	console.error(err);
 }
 
+// Create the website directory
 try {
 	fs.mkdirSync(path + '\\website');
 	console.log('Created website directory');
+} catch (err) {
+	console.error(err);
+	return;
+}
+
+// Read the template that's needed for all md pages
+let template;
+try {
+	template = fs.readFileSync(path + '/resources/template.html', 'utf8');
+	console.log('Read the template file');
 } catch (err) {
 	console.error(err);
 	return;
@@ -68,7 +79,8 @@ files.forEach((file) => {
 		if (file.includes('.md')) {
 			const content = fs.readFileSync(file, 'utf8');
 			const compiledMd = marked.parse(content.toString());
-			const newPage = template.replace('[PAGECONTENT]', compiledMd);
+			let newPage = template.replace('[PAGECONTENT]', compiledMd);
+			newPage = newPage.replace('[PAGENAME]', findPageTitle(newPage));
 			try {
 				fs.writeFileSync((file.replace('resources', 'website')).replace('.md', '.html'), newPage);
 				//file written successfully
--- resources/index.md
+++ resources/index.md
@@ -1,3 +1,5 @@
+
+<h1 class="hiddenFeature">Welcome</h1>
 <div id="siteGreeting">
     <div>Hello</div>
     <div>And</div>
